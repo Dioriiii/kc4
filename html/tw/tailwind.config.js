b67f3f8cb1e96690a97ac35e2fbc7399e594ea68
Dioriiii <chs048@naver.com>
2024-08-22 15:06:45 +0900

    add tw

---
+++ html/tw/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#0000ff",
+      },
+    },
+  },
+  plugins: [],
+};
